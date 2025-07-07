c0ff8a6136514e3ecb3d51083618ba3cff96ecaa
Кудрявцева Соня дом <kudryavtseva.sofi@yandex.ru>
2025-07-07 12:41:12 +0400

    переставила все, но надо пофиксить наслоение компонентов

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -8,24 +8,32 @@ import NamesHeart from '../assets/images/heart2.png';
 
 const Greeting = () => {
     return (
-        <div className="greeting-wrap">
-            <div className="baby-photo-block">
-                <div className='babies-container'> {/* Новый контейнер для фото детей */}
-                    <img className='babyLera' src={Lera} ></img>
-                    <img className='babySasha' src={Sasha}></img>
-                    <div className='decorate-imgs'>
-                        <img className='five-stars' src={FiveStars}></img>
-                        <img className='heart-between' src={HeartBetween}></img>
-                        <img className='two-stars' src={TwoStars}></img>
-                    </div>
-                    <div className='names-block'>
-                        <p className='names'>Александр + Валерия =</p>
-                        <img className='names-heart' src={NamesHeart}></img>
-                    </div>
-                    <div className='date-block'>
-                        <p className='date1'>24</p>
-                        <p className='date2'>июля</p>
-                        <p className='date3'>2025</p>
+        <div>
+            <div className="greeting-wrap">
+                <div className="baby-photo-block">
+                    <div className='babies-container'> {/* Новый контейнер для фото детей */}
+                        <img className='babyLera' src={Lera} ></img>
+                        <img className='babySasha' src={Sasha}></img>
+                        <div className='decorate-imgs'>
+                            <img className='five-stars' src={FiveStars}></img>
+                            <img className='heart-between' src={HeartBetween}></img>
+                            <img className='two-stars' src={TwoStars}></img>
+                        </div>
+                        <div className='names-block'>
+                            {/* <p className='names'>Александр + Валерия =</p>
+                            <img className='names-heart' src={NamesHeart}></img> */}
+                        </div>
+                        <div className='date-block'>
+                            <div className='names-block'>
+                                <p className='names'>Александр + Валерия =</p>
+                                <img className='names-heart' src={NamesHeart}></img>
+                            </div>
+                            <div className='dates'>
+                                <p className='date1'>24</p>
+                                <p className='date2'>июля</p>
+                                <p className='date3'>2025</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
